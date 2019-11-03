906102edcd4d005d861c10bb2ce6ca12063d9c4d
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-11-03 14:17:17 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "postgresql_user" {}
+
+variable "image_family" {}
+
+variable "creds" {}
+
+variable "gcs_bucket" {}
+
+variable "project" {}
+
+variable "public_ip" {}
+
+variable "frontend_dns" {}
+
+variable "region" {}
+
+variable "postgresql_database" {}
+
+variable "postgresql_password" {}
+
+variable "license_file" {}
+
+variable "postgresql_address" {}
+
+variable "dns_zone" {}
+
+variable "tfe-beta" {}
+
+variable "zone" {}
+
+variable "domain" {}
+
+variable "ssl_policy" {}
+
+variable "region" {}
+
+variable "project" {}
