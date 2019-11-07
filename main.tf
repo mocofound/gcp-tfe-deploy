4a90807588d658357e1864b2880327e577073777
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-11-07 12:44:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,14 +43,3 @@ module "tfe-beta" {
   #Base64 encoded password
   postgresql_password = "${var.postgresql_password}"
 }
-
-output "tfe-beta" {
-  value = {
-    application_endpoint         = "${module.tfe-beta.application_endpoint}"
-    application_health_check     = "${module.tfe-beta.application_health_check}"
-    installer_dashboard_password = "${module.tfe-beta.installer_dashboard_password}"
-    installer_dashboard__url     = "${module.tfe-beta.installer_dashboard_url}"
-    primary_public_ip            = "${module.tfe-beta.primary_public_ip}"
-    encryption_password          = "${module.tfe-beta.encryption_password}"
-  }
-}
