733ba13d3ff8e4e853a7f75f64e70c1c86d5bb38
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-11-07 12:43:46 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,22 @@
-variable "postgresql_user" {}
+variable "postgresql_user" {
+default = ""
+}
 
 variable "image_family" {}
 
 variable "creds" {}
 
-variable "gcs_bucket" {}
+variable "gcs_bucket" {
+default=""
+}
+
+variable "gcs_credentials" {
+default=""
+}
+
+variable "gcs_project" {
+default=""
+}
 
 variable "project" {}
 
@@ -30,4 +42,16 @@ variable "domain" {}
 
 variable "ssl_policy" {}
 
+variable "encryption_password" {}
+
 variable "subnet" {}
+
+variable "primary_count" {}
+
+variable "secondary_count" {}
+
+variable "certificate" {}
+
+variable "ca_bundle_url" {
+default=""
+}
