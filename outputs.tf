4783e0a35ab10bf58dc91a96fd9681ce96c6f0d0
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-11-07 19:25:29 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "tfe-beta" {
+output "terraform-enterprise" {
   value = {
-    application_endpoint         = "${module.tfe-beta.application_endpoint}"
-    application_health_check     = "${module.tfe-beta.application_health_check}"
-    installer_dashboard_password = "${module.tfe-beta.installer_dashboard_password}"
-    installer_dashboard__url     = "${module.tfe-beta.installer_dashboard_url}"
-    primary_public_ip            = "${module.tfe-beta.primary_public_ip}"
-    encryption_password          = "${module.tfe-beta.encryption_password}"
+    application_endpoint         = "${module.terraform-enterprise.application_endpoint}"
+    application_health_check     = "${module.terraform-enterprise.application_health_check}"
+    installer_dashboard_password = "${module.terraform-enterprise.installer_dashboard_password}"
+    installer_dashboard__url     = "${module.terraform-enterprise.installer_dashboard_url}"
+    primary_public_ip            = "${module.terraform-enterprise.primary_public_ip}"
+    encryption_password          = "${module.terraform-enterprise.encryption_password}"
   }
 }
